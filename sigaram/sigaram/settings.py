afc21277ad6c0a3e1cca806f855ff1ea005ead2f
Jegatheesan Veeramalai <jega@jega.in>
2014-11-09 11:33:25 +0530

    fix on settings.py

--- sigaram/sigaram/settings.py
+++ sigaram/sigaram/settings.py
@@ -93,6 +93,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
