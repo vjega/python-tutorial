5de54282fcd9235839e88567b365311c210911ee
Jegatheesan Veeramalai <jega@jega.in>
2014-11-15 01:59:22 +0530

    Hard Coded tamil text are moved to LC_MESSAGE now!

--- sigaram/sigaram/views.py
+++ sigaram/sigaram/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import (render, redirect)
 from django.http import HttpResponse
 
 # Create your views here.
@@ -10,3 +10,15 @@ def logout(request):
 
 def forum(request):
     return HttpResponse("Forum module will be taken up later")
+
+    
+def togglelanguage(request):
+    """
+    """
+    if request.session.get("django_language") == 'en':
+        request.session["django_language"] = 'ta'
+    elif request.session.get("django_language") == 'ta':
+        request.session["django_language"] = 'en'
+    
+    return redirect(request.META.get('HTTP_REFERER','/admin/home/'))
+     
