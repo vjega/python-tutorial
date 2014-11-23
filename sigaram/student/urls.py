00b65d9665e7fb7acd50dd48298541e32bd17781
Mala <mala@jega.in>
2014-11-23 12:59:05 +0530

    conflict fixed

--- sigaram/student/urls.py
+++ sigaram/student/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 from student import views
 
-<<<<<<< HEAD
 urlpatterns = patterns('student.views',
     url(r'^home$',                  'home',         name='home'),
     url(r'^studentresourcetype',    'studentresourcetype',
@@ -11,13 +10,4 @@ urlpatterns = patterns('student.views',
     url(r'^resourcetype',           'resourcetype', name='resourcetype'),
     url(r'^workspace',              'workspace',    name='workspace'),
     url(r'^studentslist',           'studentslist', name='studentslist'),
-=======
-urlpatterns = patterns('',
-    url(r'^home$',                  views.home,         name='home'),
-    url(r'^studentresourcetype',    views.studentresourcetype,
-                                                        name='studentresourcetype'),
-    url(r'^resourcetype',           views.resourcetype, name='resourcetype'),
-    url(r'^workspace',              views.workspace,    name='workspace'),
-    
->>>>>>> fe1a06012985c260f6f2f5f3a40ad0ac6d72cb2f
 )
