de4af28d050a51aa211f81eaa627500ebed9c327
mala <jega@jega.in>
2014-11-29 19:24:57 +0530

    add teacher forms

--- sigaram/teacher/urls.py
+++ sigaram/teacher/urls.py
@@ -4,9 +4,14 @@ from django.views.generic import RedirectView
 #from teacher import views
 
 urlpatterns = patterns('teacher.views',
-    url(r'^home$',                  'home', name='home'),
+    url(r'^home$',                  'home',    name='home'),
     url(r'^assignedresourcelist',   'assignedresourcelist',
-                                            name='assignedresourcelist'),
-    url(r'^workspace',   'workspace',       name='workspace'),
-    url(r'^workspacelist', 'workspacelist', name='workspacelist'),
+                                               name='assignedresourcelist'),
+    url(r'^workspace',   'workspace',          name='workspace'),
+    url(r'^viewworkspacelist$','viewworkspacelist',
+    									        name='viewworkspacelist'),
+    url(r'^writtenwork$', 'writtenwork',        name='writtenwork'),
+    url(r'^viewassessments$','viewassessments', name='viewassessments'),
+
+     
 )
