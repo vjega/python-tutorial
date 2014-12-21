dc6b3002c5b0228ecbfe644a9e49e36233082fe5
deepak05a <deepak05.a@gmail.com>
2014-12-22 01:04:17 +0530

    forum module added

---
+++ sigaram/api/forum/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers
+
+import viewsets
+
+router = routers.DefaultRouter()
+router.register(r'student',        	    viewsets.studentViewSet)
+router.register(r'resourceinfo',   	    viewsets.ResourceinfoViewSet)
+router.register(r'writtenworkinfo',     viewsets.WrittenworkinfoViewSet)
+router.register(r'studentworkspaceinfo',viewsets.Studentworkspaceinfo)
+router.register(r'studentnotesinfo', 	viewsets.StudentnotesinfoViewSet)
+
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+]
