b2d0b6e8b1884910d85e62f6c38f20749e45cf3b
deepak05a <deepak05.a@gmail.com>
2014-12-23 01:41:56 +0530

    forum add forms

--- sigaram/forum/views.py
+++ sigaram/forum/views.py
@@ -7,3 +7,9 @@ from forum.forms import (ForumsForm)
 
 def index(request):
     return render(request, 'portalforum/index.html', {"form" : ForumsForm.ForumsForm()})
+
+def viewtopic(request):
+   return render(request, 'portalforum/viewtopic.html')
+
+def viewpost(request):
+    return render(request, 'portalforum/viewpost.html')
