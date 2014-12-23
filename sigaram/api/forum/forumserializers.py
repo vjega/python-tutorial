6eee7b6f87a26fb1e28ee3c6d1c4e193a42d7694
mala <jega@jega.in>
2014-12-23 17:07:10 +0530

    forum add page

--- sigaram/api/forum/forumserializers.py
+++ sigaram/api/forum/forumserializers.py
@@ -7,4 +7,29 @@ class ForuminfoSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('forumid',  'forumname','totaltopic',
                   'totalpost','createdby', 'createddate',
                   'lastpostedby','lastposteddate','isdelete')
+
+class TopicinfoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Topicinfo
+        fields = ('topicid',  'forumid','topicname',
+                  'totalpost','createdby', 'createddate',
+                  'lastpostedby','lastposteddate')
+
+class PostreplyinfoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Postreplyinfo
+        fields = ('postreplyid',  'postid','postdetails',
+                  'postedby','posteddate')
+        
+class PostinfoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Postinfo
+        fields = ('postid',  'topicid','forumid',
+                  'postdetails','postedby','posteddate')
+
+class ActivitylogSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Activitylog
+        fields = ('activityid',  'loginid','pagename',
+                  'operation','usertype','stringsentence','updateddate')
         
