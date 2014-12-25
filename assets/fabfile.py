37ef5dd410cd5c732cc23272fe745a93ab3e5150
Jegatheesan Veeramalai <jega@jega.in>
2014-12-25 15:55:58 +0530

    admin home page rework

---
+++ assets/fabfile.py
@@ -0,0 +1,8 @@
+from time import sleep
+from fabric.api import env, run, cd
+env.user = 'root'
+env.hosts = ['siva-tech.com']
+def deploy():
+    print "Deploying at Siva-tech.com ..."
+    run('cd /root/portal;git pull origin master')
+    print "Deplyment complete"
