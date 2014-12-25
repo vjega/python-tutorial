cdad8085a5e5c1c214fd16d4d5011066e89b581e
Mala <mala@jega.in>
2014-12-25 21:55:38 +0530

    created the unique id generator

--- sigaram/static/js/tts.utils.js
+++ sigaram/static/js/tts.utils.js
@@ -15,3 +15,15 @@ TTS.utils.serilaizeJson =  function (form){
     })
     return JSON.stringify(indexed_array);
 };
+
+TTS.utils.guid = function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+               .toString(16)
+               .substring(1);
+  }
+  return function() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+           s4() + '-' + s4() + s4() + s4();
+  };
+})();
