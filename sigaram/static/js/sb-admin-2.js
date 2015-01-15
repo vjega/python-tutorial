bd7642ea44d91ea0ca77c78a235bfff3d199e4c5
karthik <karthik@jega.in>
2015-01-15 13:53:44 +0530

    fix on page loading

--- sigaram/static/js/sb-admin-2.js
+++ sigaram/static/js/sb-admin-2.js
@@ -1,12 +1,14 @@
 $(function() {
-
     $('#side-menu').metisMenu();
-
+    TTS.utils.ajaxloaderstart("Loading Page");
 });
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
+$(window).load(function(){
+   TTS.utils.ajaxloaderstop(); 
+})
 $(function() {
     $(window).bind("load resize", function() {
         topOffset = 50;
