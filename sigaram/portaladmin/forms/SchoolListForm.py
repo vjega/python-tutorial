c82f21ef4698e4d4e9d630e7b48ffedafe1f26ee
deepak05a <deepak05.a@gmail.com>
2015-01-23 21:50:18 +0530

    admin latest

--- sigaram/portaladmin/forms/SchoolListForm.py
+++ sigaram/portaladmin/forms/SchoolListForm.py
@@ -1,6 +1,7 @@
 from django.utils.translation import (ugettext as _,)
 from django import forms
 from crispy_forms.helper import FormHelper
+#from crispy_forms.layout import Submit
 class SchoolListForm(forms.Form):
     schoolname = forms.CharField(
         label = _("School Name"),
@@ -14,6 +15,12 @@ class SchoolListForm(forms.Form):
         required = True,
         widget = forms.TextInput({ "placeholder": _("Short Name")})
     )
+    # description = forms.CharField(
+    #     label = _("Reference"),
+    #     max_length = 100,
+    #     required = True,
+    #     widget = forms.TextInput({ "placeholder": _("Reference")})
+    # )
     def __init__(self, *args, **kwargs):
         super(SchoolListForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
