076f9c472102fc3b7df67c6a0c9a4d5d55830fc2
deepak05a <deepak05.a@gmail.com>
2015-01-28 23:40:54 +0530

    forum count

--- sigaram/api/admin/viewsets.py
+++ sigaram/api/admin/viewsets.py
@@ -1088,7 +1088,7 @@ class StickynotesResource(viewsets.ModelViewSet):
                  s.color, 
                  s.color
         ORDER BY s.createddate DESC''' %wherecond
-        #print sql;
+        print sql;
         cursor = connection.cursor()
         cursor.execute(sql)
         desc = cursor.description
@@ -1123,6 +1123,23 @@ class StickynotesResource(viewsets.ModelViewSet):
         stickynotes.createddate = time.strftime('%Y-%m-%d %H:%M:%S')
         stickynotes.save()
         return Response(request.DATA)
+
+    def retrieve(self, request, pk=None):
+        sql = '''
+        SELECT  id,
+                title 
+        FROM stickyinfo
+        WHERE id='%s'
+        ''' % (pk)
+
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        desc = cursor.description
+        result =  [
+                dict(zip([col[0] for col in desc], row))
+                for row in cursor.fetchall()
+            ]
+        return Response(result)
 
     def destroy(self, request, pk):
         models.stickynotes.objects.get(pk=pk).delete()
@@ -2823,8 +2840,8 @@ class TopicInfoViewSet(viewsets.ModelViewSet):
         return Response(result)
     
     def create(self, request):
-        print "*"*80
-        print request.user.get_full_name()
+        # print "*"*80
+        # print request.user.get_full_name()
         topics = models.Topicinfo()
         topicinfodata =  json.loads(request.DATA.keys()[0])
         topics.topicid = topicinfodata.get('topicid',0)
