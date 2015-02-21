a2347aaf69d4b769ff320fa391fb7dfaf51662f7
gbsanthos <santhosh@jega.in>
2015-02-22 00:22:54 +0530

    bug fixed

--- sigaram/api/admin/viewsets.py
+++ sigaram/api/admin/viewsets.py
@@ -3355,8 +3355,8 @@ class AssessmentInfoViewSet(viewsets.ModelViewSet):
         return Response('"msg":"delete"')
 
 class AssignmentRatingViewSet(viewsets.ModelViewSet):
-    queryset = models.Assignmentratinginfo.objects.all()
-    serializer_class = adminserializers.AssignmentratinginfoSerializer
+    queryset = models.Assignresourceinfo.objects.all()
+    serializer_class = adminserializers.AssignresourceinfoSerializer
 
     def update(self, request):
         sql = '''
