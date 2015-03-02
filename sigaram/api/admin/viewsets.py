1b1ae37cd1a5a35d9b37b3db0c272173b66131b6
deepak05a <deepak05.a@gmail.com>
2015-03-02 22:34:42 +0530

    student edit

--- sigaram/api/admin/viewsets.py
+++ sigaram/api/admin/viewsets.py
@@ -255,6 +255,7 @@ class studentViewSet(viewsets.ModelViewSet):
         student.password = studentdata.get('password')
         student.firstname = studentdata.get('firstname')
         student.schoolid = studentdata.get('schoolid')
+        student.classid = studentdata.get('classid')
         student.emailid = studentdata.get('emailid')
         student.imageurl = studentdata.get('imageurl')
         student.save()
@@ -3897,77 +3898,54 @@ class studentAssessmentInfo(viewsets.ModelViewSet):
 
     def update(self, request, pk=None):
         data = {k:v[0] for k, v in dict(request.DATA).items()}
-
-        aai = models.Assignassessmentinfo.objects.get(pk=pk)
-        aaidata =  json.loads(request.DATA.keys()[0])
-
-        if aaidata.get('issaved'):
-            aai.issaved = aaidata.get('issaved')
-        if aaidata.get('isanswerd'):
-            aai.isanswerd = aaidata.get('issaved')
-        aai.save()
-
-        if aaidata.get('alreadysaved'):
-            for k, v in dict(aaidata.get('aqaidanswer')).items():
-                aaid = models.AssignAssessmentQAInfo()
-                aaid.assessmentqaid = int(k)
-                aaid.assessmentid   = int(pk)
-                aaid.answer         = str(v)
-                aaid.save()
-
-        else:
-            for k, v in dict(aaidata.get('aqaidanswer')).items():
-                aaid = models.AssignAssessmentQAInfo.objects.get(pk=pk)
-                aaid.assessmentqaid = int(k)
-                aaid.assessmentid   = int(pk)
-                aaid.answer         = str(v)
-                aaid.save()
         
-        # ari.answertext = summer_decode(unicode(data.get('answertext')))
-
-        # if data.get('originaltext'):
-        #     ari.originaltext = summer_decode(unicode(data.get('originaltext')))
-
-        # if data.get('answerurl'):
-        #     ari.answerurl = unicode(data.get('answerurl'))
-        #     ari.isrecord = 1
-
-        # if data.get('isanswered'):
-        #     ari.isanswered = data.get('isanswered')
-        #     ari.answereddate = time.strftime('%Y-%m-%d %H:%M:%S')
-
-        # if data.get('issaved'):
-        #     ari.issaved = data.get('issaved')
+        ari = models.Assignresourceinfo.objects.get(pk=pk)
         
-        # ari.save()
-        # if data.get('spanid'):
-        #     assignedid  = pk;
-        #     spanid      = summer_decode(data.get('spanid'));
-        #     fulltext    = summer_decode(data.get('fulltext'));
-        #     orig        = summer_decode(data.get('orig'));
-        #     modified    = summer_decode(data.get('modified'));
-        #     usertype    = data.get('type');
-        #     answertext  = summer_decode(data.get('answertext'));
+        ari.answertext = summer_decode(unicode(data.get('answertext')))
 
-        #     ar = models.Editingtext()
-        #     ar.editid       = int(assignedid)
-        #     ar.spanid       = unicode(spanid)
-        #     ar.previoustext = unicode(orig)
-        #     ar.edittext     = unicode(modified)
-        #     ar.typeofresource = 0
-        #     ar.isapproved   = 0
-        #     ar.isrejected   = 0
-        #     ar.editedby     = request.user.username
-        #     ar.editeddate   = time.strftime('%Y-%m-%d %H:%M:%S')
-        #     ar.usertype     = int(usertype)
+        if data.get('originaltext'):
+            ari.originaltext = summer_decode(unicode(data.get('originaltext')))
 
-        #     ar.save()
+        if data.get('answerurl'):
+            ari.answerurl = unicode(data.get('answerurl'))
+            ari.isrecord = 1
 
-        # aldata = {}
-        # aldata['pagename']       = 'viewassignresource'
-        # aldata['operation']      = 'Insert'
-        # aldata['stringsentence'] = 'Answered For Resource'
-        # add_activitylog(request, aldata)
+        if data.get('isanswered'):
+            ari.isanswered = data.get('isanswered')
+            ari.answereddate = time.strftime('%Y-%m-%d %H:%M:%S')
+
+        if data.get('issaved'):
+            ari.issaved = data.get('issaved')
+        
+        ari.save()
+        if data.get('spanid'):
+            assignedid  = pk;
+            spanid      = summer_decode(data.get('spanid'));
+            fulltext    = summer_decode(data.get('fulltext'));
+            orig        = summer_decode(data.get('orig'));
+            modified    = summer_decode(data.get('modified'));
+            usertype    = data.get('type');
+            answertext  = summer_decode(data.get('answertext'));
+
+            ar = models.Editingtext()
+            ar.editid       = int(assignedid)
+            ar.spanid       = unicode(spanid)
+            ar.previoustext = unicode(orig)
+            ar.edittext     = unicode(modified)
+            ar.typeofresource = 0
+            ar.isapproved   = 0
+            ar.isrejected   = 0
+            ar.editedby     = request.user.username
+            ar.editeddate   = time.strftime('%Y-%m-%d %H:%M:%S')
+            ar.usertype     = int(usertype)
+
+            ar.save()
+
+        aldata = {}
+        aldata['pagename']       = 'viewassignresource'
+        aldata['operation']      = 'Insert'
+        aldata['stringsentence'] = 'Answered For Resource'
+        add_activitylog(request, aldata)
 
         return Response({'msg':True})
 
