8cf7990c7d13f8a783d522c9a4b3f17b204b3e8a
gbsanthos <santhosh@jega.in>
2015-03-03 23:08:01 +0530

    assessment

--- sigaram/api/admin/urls.py
+++ sigaram/api/admin/urls.py
@@ -64,6 +64,7 @@ router.register(r'activityassignment',    	viewsets.ActivityassignmentInfoViewSe
 router.register(r'studentassignedresource', viewsets.StudentassignedresourceInfoViewSet)
 router.register(r'assignmentanswerrating',  viewsets.AssignmentRatingViewSet)
 router.register(r'activityassessment', 		viewsets.ActivityassessmentInfoViewSet)
+router.register(r'studentassessmentinfo', 	viewsets.studentAssessmentInfoViewSet)
 router.register(r'assessmentstatistics', 	viewsets.AssessmentstatisticsInfo)
 urlpatterns = [
     url(r'^', include(router.urls)),
